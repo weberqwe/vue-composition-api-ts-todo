a730e728dcca8fb08755911c8c1f9b1754a75041
Sungjoon Park <sungjoon512@gmail.com>
2020-05-14 16:07:04 +0900

    [WIP] Vuex

---
+++ src/ts/store/index.ts
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+import Vuex, { StoreOptions } from 'vuex';
+import { nanoid } from 'nanoid';
+import { RootState, TodoItem } from './types';
+import { mutations } from './mutations';
+
+Vue.use(Vuex);
+
+const store: StoreOptions<RootState> = {
+  state: {
+    todoAllToggle: false,
+    newTodo: '',
+    beforeEditTodo: '',
+    showTooltip: false,
+    todoItems: [],
+    sortButtons: [
+      {
+        id: `sortButton_${nanoid()}`,
+        name: 'all',
+        selected: true,
+      },
+      {
+        id: `sortButton_${nanoid()}`,
+        name: 'active',
+        selected: false,
+      },
+      {
+        id: `sortButton_${nanoid()}`,
+        name: 'completed',
+        selected: false,
+      },
+    ],
+    sortCategory: 'all',
+  },
+  getters: {
+    todoItemsToShow({ sortCategory, todoItems }): TodoItem[] {
+      const isActive = sortCategory === 'active';
+      const isCompleted = sortCategory === 'completed';
+      if (!(isActive || isCompleted)) return todoItems;
+      return todoItems.filter((item) => (isActive ? !item.completed : item.completed));
+    },
+    numberOfItemsLeft({ todoItems }): number {
+      return todoItems.filter((item) => !item.completed).length;
+    },
+  },
+  mutations,
+};
+
+export default new Vuex.Store<RootState>(store);
